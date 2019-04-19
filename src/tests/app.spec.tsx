f0306105678112d0be060e0c0d7731420247d781
Kashi Pai <kashinathpai@gmail.com>
2019-04-20 02:26:34 +0530

    Update test to include navlink

--- src/tests/app.spec.tsx
+++ src/tests/app.spec.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
 import { expect } from 'chai';
-import { mount, configure } from 'enzyme';
+import { mount, configure, shallow, render } from 'enzyme';
 import { spy } from 'sinon';
 import NavigationComponent from '../components/navigation';
 import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
+import NavLink from 'reactstrap/lib/NavLink';
 
 configure({ adapter: new ReactSixteenAdapter() });
 
 spy(NavigationComponent.prototype, 'componentDidMount');
 
 describe('<NavigationComponent/>', () => {
-	it('Should have two links', () => {
+	it('Should mount', () => {
 		//expect(1).to.equal(1);
 		const wrapper = mount(<NavigationComponent />);
 		expect(NavigationComponent.prototype.componentDidMount).to.have.property('callCount', 1);
 	});
 
-	it('should fail', () => {
-		expect(1).to.equal(1);
+	it('Should contain two links', () => {
+		const wrapper = render(<NavigationComponent />);
+		expect(wrapper.find('.nav-link').length).to.equal(2);
 	});
 });
